2ca09f3fe09a80a9e3e26357b3761348d5f8b9b2
Anceps <ancspeepyalt2tg4gw@gmail.com>
2025-06-13 15:51:03 -0600

    Delete src directory

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request: Request): Promise<Response> {
-    return new Response("Hello, World!", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
