c9261cc23b9967912f0fad4d305773312d4f50c2
Anceps <ancspeepyalt2tg4gw@gmail.com>
2025-06-28 03:59:32 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,16 @@
-export default function e(){return<header >
-
+export default function Header() {
+  return (
+    <header>
       <div className="header-left">
-
-        <button id="mobile-hamburger-btn"className="mobile-hamburger-btn"type="button"aria-label="Open sidebar"title="Open sidebar menu">
-
-          <span className="bi bi-list"style={{fontSize:"2rem",color:"#DFE3F5"}}aria-hidden="true"></span>
-
+        <button id="mobile-hamburger-btn" className="mobile-hamburger-btn" type="button" aria-label="Open sidebar" title="Open sidebar menu">
+          <span className="bi bi-list" style={{fontSize: "2rem", color: "#DFE3F5"}} aria-hidden="true"></span>
         </button>
-
         <div className="logo">
-
-          <img src="/assets/favicon-96x96.png"alt="The Hardest Achievements List Logo"title="The Hardest Achievements List Logo"/>
-
+          <img src="/assets/favicon-96x96.png" alt="The Hardest Achievements List Logo" title="The Hardest Achievements List Logo" />
         </div>
-
         <h1 className="title">The Hardest Achievements List</h1>
-
       </div>
-
-      <div id="splash-text"style={{fontStyle:"italic",color:"#4d566e",marginTop:"0.2em",fontSize:"1.1em"}}></div>
-
-    </header>};
+      <div id="splash-text" style={{fontStyle: "italic", color: "#4d566e", marginTop: "0.2em", fontSize: "1.1em"}}></div>
+    </header>
+  );
+}
