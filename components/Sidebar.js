7d7bc34d4604646587ad598e8e6ccfd5f5b169bb
Anceps <ancspeepyalt2tg4gw@gmail.com>
2025-06-28 03:59:43 -0600

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,21 +1,23 @@
-import i from"next/link";export default function t(){return<nav >
+import Link from 'next/link';
 
-      <i href="/list"className="sidebar-link">Main List</i>
-
-      <i href="/timeline"className="sidebar-link">Timeline</i>
-
-      <i href="/leaderboard"className="sidebar-link">Leaderboard</i>
-
-      <i href="/submission-stats"className="sidebar-link">Submission Stats</i>
-
-      <a href="#"id="random-achievement-btn"className="sidebar-link">Random</a>
-
-      <i href="/about-us"className="sidebar-link">About Us</i>
-
-      <div style={{position:"relative",width:"100%",paddingBottom:"350px"}}>
-
-        <iframe src="https://discord.com/widget?id=1122038339541934091&theme=dark"style={{position:"absolute",top:0,left:0,width:"100%",height:"100%"}}allowTransparency="true"frameBorder="0"sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-
+export default function Sidebar() {
+  return (
+    <nav>
+      <Link href="/list" className="sidebar-link">Main List</Link>
+      <Link href="/timeline" className="sidebar-link">Timeline</Link>
+      <Link href="/leaderboard" className="sidebar-link">Leaderboard</Link>
+      <Link href="/submission-stats" className="sidebar-link">Submission Stats</Link>
+      <a href="#" id="random-achievement-btn" className="sidebar-link">Random</a>
+      <Link href="/about-us" className="sidebar-link">About Us</Link>
+      <div style={{position: "relative", width: "100%", paddingBottom: "350px"}}>
+        <iframe
+          src="https://discord.com/widget?id=1122038339541934091&theme=dark"
+          style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}
+          allowTransparency="true"
+          frameBorder="0"
+          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        ></iframe>
       </div>
-
-    </nav>};
+    </nav>
+  );
+}
