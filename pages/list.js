f5f6b8df22c6664120e21148046f440b03f76d96
Anceps <ancspeepyalt2tg4gw@gmail.com>
2025-10-09 18:50:49 -0600

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -918,7 +918,7 @@ export default function List() {
           onClick={() => setShowSidebar(false)}
         >
           <div
-            className="sidebar sidebar-mobile-open"
+            className="sidebar sidebar-mobile-modal"
             style={{
               position: "fixed",
               top: "50%",
